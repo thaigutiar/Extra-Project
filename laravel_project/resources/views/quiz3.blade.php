7ca1a87fdfd812fb55a216faa07bbabda5ab13ac
Extra <thaiguitar@windowslive.com>
2018-08-28 12:21:13 +0700

    Update 28.8.2561

---
+++ laravel_project/resources/views/quiz3.blade.php
@@ -0,0 +1,223 @@
+<html>
+<head>
+<title>Capture</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<center>
+<!-- Save for Web Slices (Capture.PNG) -->
+<table id="Table_01" width="1563" height="791" border="0" cellpadding="0" cellspacing="0">
+	<tr>
+		<td colspan="21">
+			<img src="css/images/index_01.gif" width="1562" height="18" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="18" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="20">
+			<img src="css/images/index_02.gif" width="290" height="772" alt=""></td>
+		<td colspan="9">
+			<img src="css/images/index_03.gif" width="231" height="82" alt=""></td>
+		<td colspan="11" rowspan="2">
+			<img src="css/images/index_04.gif" width="1041" height="94" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="82" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="9">
+			<img src="css/images/index_05.gif" width="231" height="12" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="12" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="11">
+			<img src="css/images/index_06.gif" width="12" height="294" alt=""></td>
+		<td colspan="2"><a href="https://www.blognone.com/feature"><img src="css/images/index_07.gif" width="81" height="36" alt=""></a></td>
+		<td colspan="4"><a href="https://www.blognone.com/topics/interview"><img src="css/images/index_08.gif" width="92" height="36" alt=""></a></td>
+		<td colspan="3"><a href="https://www.blognone.com/forum"><img src="css/images/index_09.gif" width="58" height="36" alt=""></a></td>
+		<td><a href="https://www.blognone.com/forums/blognone-jobs"><img src="css/images/index_10.gif" width="68" height="36" alt=""></a></td>
+		<td><a href="https://www.blognone.com/topics/blognone-workplace"><img src="css/images/index_11.gif" width="87" height="36" alt=""></a></td>
+		<td colspan="2"><a href="https://www.blognone.com/company-profile"><img src="css/images/index_12.gif" width="140" height="36" alt=""></a></td>
+		<td colspan="2"><a href="https://www.blognone.com/node/22962"><img src="css/images/index_13.gif" width="88" height="36" alt=""></a></td>
+		<td colspan="4" rowspan="2">
+			<img src="css/images/index_14.gif" width="646" height="51" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="36" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="15" rowspan="2">
+			<img src="css/images/index_15.gif" width="614" height="27" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="15" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2" rowspan="3">
+			<img src="css/images/index_16.gif" width="49" height="85" alt=""></td>
+		<td rowspan="4">
+			<img src="css/images/index_17.gif" width="319" height="102" alt=""></td>
+		<td rowspan="16">
+			<img src="css/images/index_18.gif" width="278" height="627" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="12" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="14"><a href="https://www.blognone.com/node/104483"><img src="css/images/index_19.gif" width="540" height="22" alt=""></a></td>
+		<td rowspan="2">
+			<img src="css/images/index_20.gif" width="74" height="73" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="22" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="14">
+			<img src="css/images/index_21.gif" width="540" height="51" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="51" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="5" rowspan="4">
+			<img src="css/images/index_22.gif" width="111" height="122" alt=""></td>
+		<td colspan="11" rowspan="3">
+			<img src="css/images/index_23.gif" width="522" height="77" alt=""></td>
+		<td rowspan="13">
+			<img src="css/images/index_24.gif" width="30" height="542" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="17" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="css/images/index_25.gif" width="319" height="11" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="11" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="11">
+			<img src="css/images/index_26.gif" width="319" height="514" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="49" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="11">
+			<img src="css/images/index_27.gif" width="522" height="45" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="45" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="2">
+			<img src="css/images/index_28.gif" width="15" height="36" alt=""></td>
+		<td colspan="2"><a href="https://www.blognone.com/node/104483"><img src="css/images/index_29.gif" width="74" height="17" alt=""></a></td>
+		<td rowspan="2">
+			<img src="css/images/index_30.gif" width="14" height="36" alt=""></td>
+		<td colspan="3"><a href="https://www.blognone.com/node/104483#comments"><img src="css/images/index_31.gif" width="80" height="17" alt=""></a></td>
+		<td colspan="9" rowspan="2">
+			<img src="css/images/index_32.gif" width="450" height="36" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="17" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<img src="css/images/index_33.gif" width="74" height="19" alt=""></td>
+		<td colspan="3">
+			<img src="css/images/index_34.gif" width="80" height="19" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="19" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="13"><a href="https://www.blognone.com/node/104476"><img src="css/images/index_35.gif" width="456" height="22" alt=""></a></td>
+		<td colspan="4" rowspan="2">
+			<img src="css/images/index_36.gif" width="189" height="71" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="22" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="13">
+			<img src="css/images/index_37.gif" width="456" height="49" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="49" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="6">
+			<img src="css/images/index_38.gif" width="123" height="103" alt=""></td>
+		<td colspan="11">
+			<img src="css/images/index_39.gif" width="522" height="103" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="103" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="4">
+			<img src="css/images/index_40.gif" width="12" height="210" alt=""></td>
+		<td colspan="16">
+			<img src="css/images/index_41.png" width="633" height="76" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="76" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="16">
+			<img src="css/images/index_42.gif" width="633" height="8" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="8" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="2">
+			<img src="css/images/index_43.gif" width="15" height="126" alt=""></td>
+		<td colspan="2"><a href="https://www.blognone.com/node/104476"><img src="css/images/index_44.gif" width="74" height="18" alt=""></a></td>
+		<td colspan="13" rowspan="2">
+			<img src="css/images/index_45.gif" width="544" height="126" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="18" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="2">
+			<img src="css/images/index_46.gif" width="74" height="108" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="1" height="108" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="css/images/spacer.gif" width="290" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="12" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="15" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="66" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="8" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="14" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="8" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="62" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="10" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="36" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="12" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="68" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="87" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="58" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="82" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="14" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="74" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="19" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="30" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="319" height="1" alt=""></td>
+		<td>
+			<img src="css/images/spacer.gif" width="278" height="1" alt=""></td>
+		<td></td>
+	</tr>
+</table>
+<!-- End Save for Web Slices -->
+</center>
+</body>
+</html>
