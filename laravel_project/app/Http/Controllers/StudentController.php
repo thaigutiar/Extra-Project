373830251c2eedd5979ff7d5495bb6574bcfde70
Extra <thaiguitar@windowslive.com>
2018-10-02 09:47:22 +0700

    Update 2.10.2561

---
+++ laravel_project/app/Http/Controllers/StudentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Studentmodel;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Studentmodel  $studentmodel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Studentmodel $studentmodel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Studentmodel  $studentmodel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Studentmodel $studentmodel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Studentmodel  $studentmodel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Studentmodel $studentmodel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Studentmodel  $studentmodel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Studentmodel $studentmodel)
+    {
+        //
+    }
+}
